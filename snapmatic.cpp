77b42a2bacd040f85a74fd6d3c850abf8e7431ca
Arino Jenynof <arino.j17@gmail.com>
2023-08-25 00:15:18 +0700

    Use non-throwing filesystem functions

--- snapmatic.cpp
+++ snapmatic.cpp
@@ -7,11 +7,12 @@ int main()
 	namespace fs = std::filesystem;
 	const fs::path userprofile = std::getenv("USERPROFILE");
 	const fs::path rgsc = userprofile / "Documents\\Rockstar Games\\GTA V\\Profiles";
+	std::error_code ec;
 
 	for (const fs::directory_entry &profile : fs::directory_iterator(rgsc))
 	{
 		const fs::path outdir = profile.path().filename();
-		fs::create_directory(outdir);
+		fs::create_directory(outdir, ec);
 
 		for (const fs::directory_entry &dirent : fs::directory_iterator(profile))
 		{
@@ -19,6 +20,9 @@ int main()
 			if (filename.string().substr(0, 4) == "PGTA")
 			{
 				const fs::path outfile = filename.string() + ".jpg";
+
+				if (fs::exists(outdir / outfile, ec))
+					continue;
 
 				std::ifstream inp(dirent.path(), std::ios::binary);
 				std::ofstream out(outdir / outfile, std::ios::binary);
