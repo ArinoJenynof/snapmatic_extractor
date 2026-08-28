543b21f63423227ef5b44657bb82874f24760365
Arino Jenynof <arino.j17@gmail.com>
2026-08-28 21:30:37 +0700

    Don't throw up if profile dir doesn't exists. - Use C++23 now for the sake of using C++23 (this is a toy program) - Now need to set output folder. JPGs will be stored there.

--- snapmatic.cpp
+++ snapmatic.cpp
@@ -1,39 +1,44 @@
-#include <iostream>
-#include <fstream>
+#include <print>
 #include <filesystem>
-#include <array>
+#include <fstream>
 
-int main()
+int main(int argc, char *argv[])
 {
-	using namespace std::filesystem;
+	if (argc != 2)
+	{
+		std::println("Usage: snapmatic <output folder>");
+		std::println("\t<output folder>\tFolder to place extracted JPGs");
+		return -1;
+	}
 
-	const path USERPROFILE { std::getenv("USERPROFILE") };
+	const std::filesystem::path userprofile { std::getenv("USERPROFILE") };
+	const std::filesystem::path outdir { argv[1] };
 	const std::array profile_dirs {
-		USERPROFILE / "Documents" / "Rockstar Games" / "GTA V" / "Profiles",
-		USERPROFILE / "Documents" / "Rockstar Games" / "GTAV Enhanced" / "Profiles"
+		userprofile / "Documents" / "Rockstar Games" / "GTA V" / "Profiles",
+		userprofile / "Documents" / "Rockstar Games" / "GTAV Enhanced" / "Profiles"
 	};
 
-	for (const path& profile_dir : profile_dirs)
+	std::error_code ec {};
+	std::filesystem::create_directories(outdir, ec);
+	for (const std::filesystem::path& profile_dir : profile_dirs)
 	{
-		for (const directory_entry& profile : directory_iterator { profile_dir })
+		std::println("Searching {}", profile_dir.string());
+		for (const std::filesystem::directory_entry& profile_id : std::filesystem::directory_iterator(profile_dir, ec))
 		{
-			std::error_code ec {};
-			const path outdir { profile.path().filename() };
-			create_directories(outdir, ec);
-
-			for (const directory_entry& pic : directory_iterator { profile })
+			std::println("\tFound profile {}", profile_id.path().filename().string());
+			for (const std::filesystem::directory_entry& e : std::filesystem::directory_iterator(profile_id, ec))
 			{
-				if (pic.path().filename().string().substr(0, 4) == "PGTA")
+				if (e.path().filename().string().substr(0, 4) == "PGTA")
 				{
-					const path outfile { outdir / (pic.path().filename().string() + ".jpg") };
-					if (exists(outfile, ec))
-						continue;
-					const path infile { pic.path() };
+					const std::filesystem::path outfile { outdir / (e.path().filename().string() + ".jpg") };
+					std::println("\t\tExtracting to {}", outfile.string());
 
-					std::ifstream snap_in { infile, std::ios::binary };
-					std::ofstream snap_out { outfile, std::ios::binary };
-					snap_in.seekg(292);
-					snap_out << snap_in.rdbuf();
+					std::ifstream snap_inp(e.path(), std::ios::binary);
+					std::ofstream snap_out(outfile, std::ios::binary);
+
+					// Bytes 0-291 are Rockstar's metadata, possibly date taken and latitude longitude
+					snap_inp.seekg(292);
+					snap_out << snap_inp.rdbuf();
 				}
 			}
 		}
